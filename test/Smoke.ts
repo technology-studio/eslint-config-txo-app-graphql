4fd2900f48646ec9b82fac4e4aa37346de0b4828
Erik Slovak <erik.slovak@technologystudio.sk>
2025-02-06 14:02:49 +0100

    feat: initial implementation

---
+++ test/Smoke.ts
@@ -0,0 +1,8 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2025-02-06T13:38:56+01:00
+ * @Copyright: Technology Studio
+**/
+
+// eslint-disable-next-line @stylistic/semi -- test
+export const NoStylisticSemi = (): undefined => undefined;
